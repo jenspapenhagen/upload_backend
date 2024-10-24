181f689f9ad6ba1443fa09992d5c0c1d43a25afa
jenspapenhagen <jens.papenhagen@web.de>
2024-10-24 21:45:42 +0200

    first test with upload_frontend

--- src/main/java/de/papenhagen/FilesController.java
+++ src/main/java/de/papenhagen/FilesController.java
@@ -20,6 +20,7 @@ import org.jboss.resteasy.reactive.server.multipart.MultipartFormDataInput;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
