407d65853d6ccd9ef082251d195893d25d2965a8
jenspapenhagen <jens.papenhagen@web.de>
2024-10-24 19:31:15 +0200

    adding first steps to getting the right filename

--- src/main/java/de/papenhagen/FilesController.java
+++ src/main/java/de/papenhagen/FilesController.java
@@ -3,6 +3,7 @@ package de.papenhagen;
 import io.quarkus.qute.Location;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import jakarta.activation.MimetypesFileTypeMap;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
@@ -10,6 +11,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.server.multipart.FileItem;
@@ -17,15 +19,16 @@ import org.jboss.resteasy.reactive.server.multipart.FormValue;
 import org.jboss.resteasy.reactive.server.multipart.MultipartFormDataInput;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Objects.nonNull;
+
 @ApplicationScoped
 @Produces(MediaType.TEXT_HTML)
-
+@Path("files.html")
 public class FilesController {
 
     private static final Logger LOG = Logger.getLogger(FilesController.class);
@@ -41,14 +44,7 @@ public class FilesController {
     @Location("successUpload.html")
     Template successTemplate;
 
-    @Path("files.html")
-    @GET
-    public TemplateInstance get() {
-        return template.instance();
-    }
-
     @POST
-    @Path("/upload")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public TemplateInstance upload(final MultipartFormDataInput input) throws IOException {
 
@@ -60,14 +56,17 @@ public class FilesController {
             final FileItem fileItem = inputPart.getFileItem();
             LOG.info("filesize: " + fileItem.getFileSize() + " Bytes");
 
-            final String contentType = Files.probeContentType(fileItem.getFile());
-            if (!WHITELIST.contains(contentType)) {
+            final MultivaluedMap<String, String> headers = inputPart.getHeaders();
+            final String fileName = getFileName(headers);
+            LOG.info("fileName: " + fileName);
+
+            final String fileExtension = getFileExtension(fileName);
+            LOG.info("fileNameParts[1]: " + fileExtension);
+
+            if (!WHITELIST.contains(fileExtension)) {
                 LOG.error("File not allowed");
                 return template.instance();
             }
-
-            //WARNING: we are losing the file postfix.
-            final String fileName = fileItem.getFile().toFile().getName();
 
             //move the given file into the uploadFile folder
             final java.nio.file.Path currentWorkingDir = Paths.get("").toAbsolutePath();
@@ -84,6 +83,48 @@ public class FilesController {
         }
 
         return successTemplate.instance();
+    }
+
+    @GET
+    public TemplateInstance get() {
+        return template.instance();
+    }
+
+
+    /**
+     * This method gives back the fileName.
+     *
+     * header sample
+     * {
+     * 	Content-Type=[image/png],
+     * 	Content-Disposition=[form-data; name="file"; filename="filename.extension"]
+     * }
+     **/
+    private String getFileName(MultivaluedMap<String, String> header) {
+
+        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
+
+        for (String filename : contentDisposition) {
+            if ((filename.trim().startsWith("filename"))) {
+
+                String[] name = filename.split("=");
+
+                String finalFileName = name[1].trim().replaceAll("\"", "");
+                return finalFileName;
+            }
+        }
+        return "unknown";
+    }
+
+    private String getFileExtension(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        int dotIndex = filename.lastIndexOf(".");
+        if (dotIndex >= 0) {
+            return filename.substring(dotIndex + 1);
+        }
+        return "";
     }
 
 }
